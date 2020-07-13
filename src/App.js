81e69a05acc65d3c7402753bcdb9e93de1beec2c
Shadab420 <shadab_anwar@outlook.com>
2020-07-13 11:40:35 +0600

    first commit and configured bulma css

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './css/myBulma.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Programming Hero Web Course</h1>
+    </div>
+  );
+}
+
+export default App;
